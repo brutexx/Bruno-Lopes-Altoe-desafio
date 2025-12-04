2f34d80f0717f876ce1e43b85d8fcb87ee56c326
brutexx <79064566+brutexx@users.noreply.github.com>
2025-12-03 23:01:14 -0300

    Lista com dados mock renderizada
    Não tenho certeza se "renderizar lista" significa <ul>, ou se eu posso utilizar
    <table>. Assumi que sim, parece a escolha certa para vários dados com as mesmas
    propriedades.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let currentTheme = 'light';
 // ---
 const THEME_SWITCHER = document.getElementById('theme-switcher');
 const ROOT = document.documentElement;
+const TRANSACTIONS_TABLE = document.getElementById('body-transaction-table');
 
 // ---
 // FUNÇÕES AUXILIARES 
@@ -22,8 +23,49 @@ const ROOT = document.documentElement;
  * @returns {string} A data formatada.
  */
 function formatDate(dateString) {
-    // A longo prazo, parece mais fácil de modificar.
     return new Date(dateString).toLocaleDateString("pt-BR");
+}
+
+/**
+ * Formata um número para o padrão de dinheiro brasileiro (R$X.XXX,XX).
+ * @param {number} currencyNumber - O número a formatar.
+ * @returns {string} O número formatado.
+ */
+function formatCurrency(currencyNumber) {
+    return currencyNumber.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+}
+
+/**
+ * Coloca uma transação na tabela de transações (tela principal).
+ * @param {Object} transaction - A transação com propriedades preenchidas.
+ */
+function renderRow(transaction) {
+    const tr = document.createElement("tr");
+
+    if (transaction.type === "income" || transaction.type === "expense")
+        tr.classList.add(transaction.type);
+
+    // Célula de Valor
+    const amountTd = document.createElement("td");
+    amountTd.textContent = formatCurrency(transaction.amount);
+
+    // Célula de Data
+    const dateTd = document.createElement("td");
+    dateTd.textContent = formatDate(transaction.date);
+
+    // Célula de Tipo
+   const typeTd = document.createElement("td");
+    typeTd.textContent = transaction.type;
+
+    // Coloca as células na tabela
+    tr.appendChild(amountTd);
+    tr.appendChild(dateTd);
+    tr.appendChild(typeTd);
+
+    TRANSACTIONS_TABLE.appendChild(tr);
 }
 
 // ---
@@ -35,13 +77,13 @@ function formatDate(dateString) {
  */
 THEME_SWITCHER.addEventListener('click', () => {
     ROOT.dataset.theme = ROOT.dataset.theme === "dark" ? "light" : "dark";
-    console.log(ROOT.dataset.theme);
 });
 
 /**
  * Função de inicialização da aplicação. A "main"
  */
 function init() {
+    transactions.forEach(renderRow);
 }
 
 // Inicia a aplicação
