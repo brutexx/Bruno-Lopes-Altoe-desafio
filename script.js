e289574ae2c3802e046145fbe98acb9fa5d12f6d
brutexx <79064566+brutexx@users.noreply.github.com>
2025-12-05 17:50:13 -0300

    Filtro implementado.
    Também modifiquei um pouco a aparência da tabela, principalmente.

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ let currentTheme = 'light';
 // ---
 const THEME_SWITCHER = document.getElementById('theme-switcher');
 const ROOT = document.documentElement;
-const TRANSACTIONS_TABLE = document.getElementById('main-transaction-table');
+const TRANSACTIONS_TABLE_BODY = document.getElementById('transaction-table-body');
 const FORM = document.getElementById("main-form");
+const SEARCH_INPUT = document.getElementById("transaction-table-search-input");
 
 // ---
 // FUNÇÕES AUXILIARES 
@@ -27,9 +28,11 @@ function formatDate(dateString) {
     let date;
 
     if (!dateString.includes('T')) {
+        // Ex: "2025-12-05"
         const [y, m, d] = dateString.split('-').map(Number);
         date = new Date(y, m - 1, d);
     } else {
+        // Ex: "2025-12-05T00:00:00.000Z"
         date = new Date(dateString);
     }
 
@@ -38,10 +41,11 @@ function formatDate(dateString) {
 
 /**
  * Formata um número para o padrão de dinheiro brasileiro (R$X.XXX,XX).
- * @param {number} currencyNumber - O número a formatar.
+ * @param {string|number} currencyNumber - O número a formatar.
  * @returns {string} O número formatado.
  */
 function formatCurrency(currencyNumber) {
+    currencyNumber = Number(currencyNumber);
     return currencyNumber.toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL',
@@ -53,7 +57,7 @@ function formatCurrency(currencyNumber) {
  * @param {Object} transaction - A transação com propriedades preenchidas.
  */
 function addTransactionRow(transaction) {
-    const { amount, date, type } = transaction;
+    const { amount, date, type, description } = transaction;
     const tr = document.createElement('tr');
 
     if (type === 'income' || type === 'expense')
@@ -67,16 +71,17 @@ function addTransactionRow(transaction) {
     const dateTd = document.createElement('td');
     dateTd.textContent = formatDate(date);
 
-    // Célula de Tipo
-    const typeTd = document.createElement('td');
-    typeTd.textContent = type;
+    // Célula de Descrição
+    const descriptionTd = document.createElement('td');
+    descriptionTd.dataset.searchTarget = ''; // Coluna usada para busca
+    descriptionTd.textContent = description;
 
     // Coloca as células na linha
     tr.appendChild(amountTd);
     tr.appendChild(dateTd);
-    tr.appendChild(typeTd);
+    tr.appendChild(descriptionTd);
 
-    TRANSACTIONS_TABLE.appendChild(tr);
+    TRANSACTIONS_TABLE_BODY.appendChild(tr);
 }
 
 // ---
@@ -90,12 +95,33 @@ THEME_SWITCHER.addEventListener('click', () => {
     ROOT.dataset.theme = ROOT.dataset.theme === 'dark' ? 'light' : 'dark';
 });
 
+/**
+ * Adiciona novas transações à tabela de transações.
+ */
 FORM.addEventListener('submit', (event) => {
     // Página não recarrega
     event.preventDefault();
     const formData = new FormData(FORM);
     const transaction = Object.fromEntries(formData.entries());
     addTransactionRow(transaction);
+});
+
+/**
+ * Lida com a filtragem das linhas da tabela.
+ */
+SEARCH_INPUT.addEventListener('input', () => {
+    const query = SEARCH_INPUT.value.toLowerCase().trim();
+    const rows = TRANSACTIONS_TABLE_BODY.querySelectorAll("tr");
+
+    console.log(rows);
+
+    rows.forEach(row => {
+        const cell = row.querySelector('[data-search-target]');
+        const cellText = cell.textContent.toLowerCase();
+        const matches = cellText.includes(query);
+
+        row.classList.toggle('hidden', !matches);
+    });
 });
 
 /**
